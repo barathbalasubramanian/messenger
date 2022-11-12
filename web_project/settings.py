8a794a05b2c1c719b8983529ef1ab12557ffd57d
barathbalasubramanian <barathkumar.b2411@gmail.com>
2022-11-12 21:25:55 +0530

    AUTH TOKEN

--- web_project/settings.py
+++ web_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(g#&=e0jy()ce*(j7%8a!t^6@yj9)=9h+z11kybp!mto&zkquy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127:0:0:1', 'msghere.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127:0:0:1', 'msghere.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
