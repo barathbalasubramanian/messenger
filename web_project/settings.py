189fa83c5e976f32ffa5f7467a3e6813455afe37
barath kumar <barathkumar.b2411@gmail.com>
2023-01-03 15:48:59 +0530

    deploy problem css

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-(g#&=e0jy()ce*(j7%8a!t^6@yj9)=9h+z11kybp!mto&zkquy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','web-production-66a9.up.railway.app']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = []
-    
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
