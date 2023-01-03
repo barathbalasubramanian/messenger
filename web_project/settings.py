f1ae650806610c4cc82faa2cb4d0c9ef5821c66a
barath kumar <barathkumar.b2411@gmail.com>
2023-01-03 15:44:39 +0530

    deploy problem css

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-(g#&=e0jy()ce*(j7%8a!t^6@yj9)=9h+z11kybp!mto&zkquy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://0.0.0.0:6017','https://web-production-66a9.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','web-production-66a9.up.railway.app']
 # ALLOWED_HOSTS = []
-
+    
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 # MEDIA_URL = '/images/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-# MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
 
 # Default primary key field type
