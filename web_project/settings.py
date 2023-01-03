21c9b7cd88d407613c2faaf0354bd6d02ab8ddb5
barath kumar <barathkumar.b2411@gmail.com>
2023-01-03 15:26:43 +0530

    deploy problem css

--- web_project/settings.py
+++ web_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
