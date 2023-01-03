8d9b11bfacebc5a7c32608617175b8c53f4800e8
barath kumar <barathkumar.b2411@gmail.com>
2023-01-03 15:04:08 +0530

    deploy problem css

--- web_project/settings.py
+++ web_project/settings.py
@@ -131,3 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedmanifestStaticFilesStorage'
