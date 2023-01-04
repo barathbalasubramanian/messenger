020ccbce2f22c5242f70759958d647b42cabe5b9
barath kumar <barathkumar.b2411@gmail.com>
2023-01-04 11:57:02 +0530

    remove css

--- web_project/settings.py
+++ web_project/settings.py
@@ -134,9 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
