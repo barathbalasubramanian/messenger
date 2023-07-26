b4aecfeea7680e4ca4ae3299d67148d51ecda543
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-07-26 21:49:16 +0530

    pyanywhere

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'firstapp.apps.FirstappConfig',
     
     'django_unicorn'
@@ -128,9 +129,10 @@ STATIC_URL = '/static/'
 
 # MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
